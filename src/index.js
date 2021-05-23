713d303d5f332d0cb32d63fedfa11f6ce8a2548e
Pravesh-Kumar <kumpravesh99@gmail.com>
2021-05-23 14:43:06 +0530

    edited index

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import App from "./components/App";
 
 ReactDOM.render(
     <React.Fragment>
-        <div data-ns-test="project-name">Weather App</div>
-<div data-ns-tset="project-description">To get the details of weather</div>
+        <h1 data-ns-test="project-name">Weather App</h1>
+<h2 data-ns-tset="project-description">To get the details of weather</h2>
     </React.Fragment>,
 
     document.getElementById("root")
