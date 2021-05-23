c7ac6655ed759d37bc72efefb29fd6fb85b9c30d
Pravesh-Kumar <kumpravesh99@gmail.com>
2021-05-23 17:57:20 +0530

    edited App.js and index.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,10 @@ import '../styles/App.css';
 const App = () => {
   return  (
     <div id="main">
-
+<div data-ns-test = "project-name">Weather App</div>
+    
+   <div data-ns-test="project-description">Dream Projects</div> 
+    
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -5,10 +5,7 @@ import App from "./components/App";
 
 
 ReactDOM.render(
-    <React.Fragment>
-        <h1 data-ns-test="project-name">Weather App</h1>
-<h2 data-ns-tset="project-description">To get the details of weather</h2>
-    </React.Fragment>,
+    <App/>,
 
     document.getElementById("root")
     );
