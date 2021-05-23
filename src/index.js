f4549763d8179b8798bb0d6c0a0fec41ff57d6bc
Pravesh-Kumar <kumpravesh99@gmail.com>
2021-05-23 14:12:59 +0530

    Added comma

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ ReactDOM.render(
     <React.Fragment>
         <div data-ns-test="project-name">Weather App</div>
 <div data-ns-tset="project-description">To get the details of weather</div>
-    </React.Fragment>
+    </React.Fragment>,
 
     document.getElementById("root")
     );
