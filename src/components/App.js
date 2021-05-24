5fdc5ccd8e5be5e9d350502e4896dc0752c72372
Pravesh-Kumar <kumpravesh99@gmail.com>
2021-05-24 12:28:53 +0530

    edited App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,11 @@
-
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+const App = () => {
+  return (
+    <div id="main">
+      <h1 data-ns-test= "project-name">Pet Adoption Website</h1>
+      <p data-ns-test="project-description">This website will help people to adopt pets instead of buying them.</p>
+    </div>
+  )
+}
+export default App;
